5be891c2b602540f8b73f5c5181581e3cdc954e3
the-code-machine <coboxgames@gmail.com>
2026-03-24 16:12:58 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // !! WARN !!
+    // Disabling type checking is not recommended, but it can be useful in certain scenarios.
+    // Make sure to run `tsc --noEmit` separately to catch type errors during development.
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
